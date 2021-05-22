0c16480b1242f93bda0f54ed07b9c0936b1b9ee8
paulaavn <paulaalbovn@gmail.com>
2021-05-22 12:22:04 +0200

    add html

--- src/js/00-00Search.js
+++ src/js/00-00Search.js
@@ -1,10 +1,3 @@
 // fetch('https://api.rand.fun/games/rockpaperscissorslizardspock')
 //   .then(response => response.json())
 //   .then(data => document.body.innerHTML = data.result);
-
-
-
-
-
-
-//# sourceMappingURL=main.js.map
